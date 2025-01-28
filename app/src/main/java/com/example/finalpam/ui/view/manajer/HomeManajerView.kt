d84c64d5f893cc24238b23670f0c45a141cfe384
dewikhulbi <dewikhlb20@gmail.com>
2025-01-28 19:39:59 +0700

    Menampilkan daftar data "Manajer" yang diambil dari database atau API.

---
+++ app/src/main/java/com/example/finalpam/ui/view/manajer/HomeManajerView.kt
@@ -0,0 +1,232 @@
+package com.example.finalpam.ui.view.manajer
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalpam.model.Manajer
+import com.example.finalpam.ui.CostumeWidget.CostumeTopAppBar
+import com.example.finalpam.ui.Navigasi.DestinasiNavigasi
+import com.example.finalpam.ui.view.manajer.DestinasiHomeManajer
+import com.example.finalpam.ui.viewmodel.PenyediaViewModel
+import com.example.finalpam.ui.viewmodel.manajer.HomeManajerUiState
+import com.example.finalpam.ui.viewmodel.manajer.HomeManajerViewModel
+import com.example.finalproject.R
+
+object  DestinasiHomeManajer  : DestinasiNavigasi {
+    override val  route  =  "homemanajer"
+    override val  titleRes  =  "Home Manajer"
+}
+
+@OptIn ( ExperimentalMaterial3Api ::class )
+@Composable
+fun  HomeManajerScreen (
+    navigateToItemEntry :  ()  ->  Unit,
+    modifier : Modifier =  Modifier,
+    onDetailClick :  ( String )  ->  Unit  =  {},
+    viewModel : HomeManajerViewModel =  viewModel ( factory =  PenyediaViewModel .Factory)
+) {
+    val  scrollBehavior  =  TopAppBarDefaults . enterAlwaysScrollBehavior ()
+    Scaffold (
+        modifier =  modifier . nestedScroll ( scrollBehavior .nestedScrollConnection),
+        topBar =  {
+            CostumeTopAppBar (
+                title =  DestinasiHomeManajer .titleRes,
+                canNavigateBack =  false ,
+                scrollBehavior =  scrollBehavior ,
+                onRefresh =  {
+                    viewModel . getMnjr ()
+                }
+            )
+        } ,
+        floatingActionButton =  {
+            FloatingActionButton (
+                onClick =  navigateToItemEntry ,
+                shape =  MaterialTheme . shapes .medium,
+                modifier =  Modifier . padding ( 18 . dp )
+            )  {
+                Icon ( imageVector =  Icons .Default. Add ,  contentDescription =  "Add Kontak" )
+
+            }
+        } ,
+    )  {  innerPadding  ->
+        HomeStatus (
+            homeUiState =  viewModel .mnjrUIState,
+            retryAction =  {  viewModel . getMnjr ()  } ,  modifier =  Modifier . padding ( innerPadding ),
+            onDetailClick =  onDetailClick ,  onDeleteClick =  {
+                viewModel . deleteMnjr ( it .id_manajer   )
+                viewModel . getMnjr ()
+            }
+        )
+
+    }
+
+
+}
+
+@Composable
+fun  HomeStatus (
+    homeUiState : HomeManajerUiState,
+    retryAction :  ()  ->  Unit,
+    modifier : Modifier =  Modifier,
+    onDeleteClick :  (Manajer)  ->  Unit  =  {},
+    onDetailClick :  ( String )  ->  Unit
+) {
+
+    when  ( homeUiState ) {
+        is  HomeManajerUiState. Loading  ->  OnLoading ( modifier =  modifier . fillMaxSize ())
+
+        is  HomeManajerUiState. Success  ->
+            if  ( homeUiState .manajer. isEmpty ()){
+                return  Box ( modifier =  modifier . fillMaxSize (),  contentAlignment =  Alignment .Center)  {
+                    Text ( text =  "Tidak ada data Kontak"  )
+
+                }
+            } else  {
+                MnjrLayout (
+
+                    manajer =  homeUiState .manajer,  modifier =  modifier . fillMaxWidth (),
+                    onDetailClick =  {
+                        onDetailClick ( it .id_manajer)
+                    },
+                    onDeleteClick =  {
+                        onDeleteClick ( it )
+                    }
+                )
+            }
+        is  HomeManajerUiState. Error  ->  OnError ( retryAction ,  modifier =  modifier . fillMaxSize ())
+    }
+
+}
+
+@Composable
+fun  OnLoading ( modifier : Modifier =  Modifier ) {
+    Image (
+        modifier =  modifier.size ( 200 . dp ),
+        painter =  painterResource ( R.drawable.loading_img),
+        contentDescription =  stringResource ( R.string.loading )
+    )
+}
+
+@Composable
+fun  OnError ( retryAction :  ()  ->  Unit ,  modifier : Modifier =  Modifier ) {
+    Column (
+        modifier =  modifier ,
+        verticalArrangement =  Arrangement .Center,
+        horizontalAlignment =  Alignment .CenterHorizontally
+    )  {
+        Image (
+            painter =  painterResource ( id =  R . drawable . ic_connection_error ),  contentDescription =  ""
+        )
+        Text ( text =  stringResource ( R . string . loading_failed ),  modifier =  Modifier . padding ( 16 . dp ))
+        Button ( onClick =  retryAction )  {
+            Text ( stringResource ( R . string . retry ))
+        }
+    }
+}
+
+@Composable
+fun  MnjrLayout (
+    manajer :  List <Manajer>,
+    modifier : Modifier =  Modifier,
+    onDetailClick :  (Manajer)  ->  Unit,
+    onDeleteClick :  (Manajer)  ->  Unit  =  {}
+) {
+    LazyColumn (
+        modifier =  modifier ,
+        contentPadding =  PaddingValues ( 16 . dp ),
+        verticalArrangement =  Arrangement . spacedBy ( 16 . dp )
+    )  {
+        items(manajer) { mnjr ->
+            MnjrCard(
+                manajer = mnjr,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(mnjr) },
+                onDeleteClick = {
+                    onDeleteClick(mnjr)
+                }
+            )
+        }
+
+    }
+
+}
+
+@Composable
+fun  MnjrCard (
+    manajer : Manajer,
+    modifier : Modifier =  Modifier,
+    onDeleteClick :  (Manajer)  ->  Unit  =  {}
+) {
+    Card (
+        modifier =  modifier ,
+        shape =  MaterialTheme . shapes .medium,
+        elevation =  CardDefaults . cardElevation ( defaultElevation =  8 . dp )
+    )  {
+        Column (
+            modifier =  Modifier . padding ( 16 . dp ),
+            verticalArrangement =  Arrangement . spacedBy ( 8 . dp )
+        )  {
+            Row (
+                modifier =  Modifier . fillMaxWidth (),
+                verticalAlignment =  Alignment .CenterVertically
+            )  {
+                Text (
+                    text =  manajer .id_manajer,
+                    style =  MaterialTheme . typography .titleLarge,
+
+                    )
+                Spacer ( Modifier . weight ( 1f ))
+                IconButton ( onClick =  {  onDeleteClick ( manajer )  } )  {
+                    Icon (
+                        imageVector =  Icons .Default. Delete ,
+                        contentDescription =  null ,
+                    )
+                }
+                Text (
+                    text =  manajer .nama_manajer,
+                    style =  MaterialTheme . typography .titleMedium
+                )
+            }
+
+            Text (
+                text =  manajer .kontak_manajer,
+                style =  MaterialTheme . typography .titleMedium
+            )
+        }
+
+    }
+}

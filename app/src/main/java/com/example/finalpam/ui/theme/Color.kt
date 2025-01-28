792be8ca8d03c39329236e7558cea2d3829965a4
dewikhulbi <dewikhlb20@gmail.com>
2025-01-28 19:59:03 +0700

    finish

---
+++ app/src/main/java/com/example/finalpam/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.finalpam.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF1F35CC)
+val PurpleGrey80 = Color(0xFF342198)
+val Pink80 = Color(0xFF384AB4)
+
+val Purple40 = Color(0xFF1733C2)
+val PurpleGrey40 = Color(0xFF55555B)
+val Pink40 = Color(0xFF4E68DC)
+
+val BackgroundLight = Color(0xFF2A64A8)
+val BackgroundDark = Color(0xFFBCC4CB)
